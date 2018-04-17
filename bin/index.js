22acddaa4fb09c35f5194146bb07cb6bd160bfa7
Hujimiya <hujimiya@outlook.com>
2018-04-16 20:50:11 -0700

    ..

---
+++ bin/index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+const process = require("process");
+const fs = require("fs");
+const path = require("path");
+const is_valid_path = require("is-valid-path");
+
+let cwd = process.cwd();
+
+debugger
+
